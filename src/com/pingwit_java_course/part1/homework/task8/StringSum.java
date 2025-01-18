4870d53a8d5a4b8add877140753a7d5fe56d1996
Alexander Fomin <rezviybelorus@gmail.com>
2025-01-18 19:50:15 +0100

    Check homework 1,2

--- src/com/pingwit_java_course/part1/homework/task8/StringSum.java
+++ src/com/pingwit_java_course/part1/homework/task8/StringSum.java
@@ -2,6 +2,6 @@ package com.pingwit_java_course.part1.homework.task8;
 
 public class StringSum {
     public static void main(String[] args) {
-        System.out.println(args[0]+args[1]);
+        System.out.println(args[0]+args[1]); // отформатируй код
     }
 }
