f2f2729457aa9ff7f28ea6ac6485356fc59dc1a6
Ivan Khomich <khomichivan03@gmail.com>
2025-01-18 20:10:35 +0100

    Reformated code in Part1 homework package for task7 and task8

--- src/com/pingwit_java_course/part1/homework/task7/RemainderOfDivision.java
+++ src/com/pingwit_java_course/part1/homework/task7/RemainderOfDivision.java
@@ -4,6 +4,6 @@ public class RemainderOfDivision {
     public static void main(String[] args) {
         int num_1 = 5;
         int num_2 = 3;
-        System.out.println(num_1%num_2); // отформатируй код
+        System.out.println(num_1 % num_2);
     }
 }
