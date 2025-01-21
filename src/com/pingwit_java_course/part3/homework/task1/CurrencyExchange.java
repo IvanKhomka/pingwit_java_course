de56032921c1a56b6d7ab1a26d67897ba5326d9b
Ivan Khomich <khomichivan03@gmail.com>
2025-01-21 11:17:46 +0100

    Changed naming for Rates and Scanner

--- src/com/pingwit_java_course/part3/homework/task1/CurrencyExchange.java
+++ src/com/pingwit_java_course/part3/homework/task1/CurrencyExchange.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class CurrencyExchange {
     public static void main(String[] args) {
 
-        double PlnRate = 4.1; // plnRate - стиль называется camelCase и первое слово всегда с маленькой буквы, даже если это имя собственное
-        double EurRate = 0.9; // eurRate
+        double plnRate = 4.1;
+        double eurRate = 0.9;
 
-        Scanner sc = new Scanner(System.in); // sc - с сокращениями стоит быть очень аккуратным, лучше просто scanner
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter currency Id: 1 for PLN, 2 for EUR");
-        int currencyId = sc.nextInt();
+        int currencyId = scanner.nextInt();
 
         System.out.println("Enter USD amount to exchange");
-        int amount = sc.nextInt();
+        int amount = scanner.nextInt();
 
         switch (currencyId) {
-            case 1 -> System.out.println("You received " + amount * PlnRate + " PLN");
-            case 2 -> System.out.println("You received " + amount * EurRate + " EUR");
+            case 1 -> System.out.println("You received " + amount * plnRate + " PLN");
+            case 2 -> System.out.println("You received " + amount * eurRate + " EUR");
 
             default -> System.out.println("Your bank can't exchange that type of currency");
         }
