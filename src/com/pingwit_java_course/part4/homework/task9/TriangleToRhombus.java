5458d4db5a78f0d281c2e71ee8edd3de1524c472
Ivan Khomich <khomichivan03@gmail.com>
2025-01-26 22:34:59 +0100

    Reformated code

--- src/com/pingwit_java_course/part4/homework/task9/TriangleToRhombus.java
+++ src/com/pingwit_java_course/part4/homework/task9/TriangleToRhombus.java
@@ -4,7 +4,7 @@ public class TriangleToRhombus {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 4; i++) {
-            for (int k = 1; k <= 4-i; k++) {
+            for (int k = 1; k <= 4 - i; k++) {
                 System.out.print(" ");
             }
             for (int j = 1; j <= i; j++) {
@@ -16,7 +16,7 @@ public class TriangleToRhombus {
             System.out.println();
         }
         for (int i = 1; i <= 4; i++) {
-            for (int k = 2; k >= 4-i; k--) {
+            for (int k = 2; k >= 4 - i; k--) {
                 System.out.print(" ");
             }
             for (int j = 4; j >= i; j--) {
