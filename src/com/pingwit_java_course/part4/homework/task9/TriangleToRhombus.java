a3caab2e7318376d66e0b0fd10bbde1969eabe85
Ivan Khomich <khomichivan03@gmail.com>
2025-02-02 00:20:10 +0100

    added 3 loops solution for task 9 in part 4

--- src/com/pingwit_java_course/part4/homework/task9/TriangleToRhombus.java
+++ src/com/pingwit_java_course/part4/homework/task9/TriangleToRhombus.java
@@ -2,42 +2,28 @@ package com.pingwit_java_course.part4.homework.task9;
 
 public class TriangleToRhombus {
     public static void main(String[] args) {
-/*
-1. Цифра 4 повторяется во всех циклах, это хороший кандидат на переменную int size = 4;
-Тогда можно регулировать размер в 1 месте и не страдать
 
-2. Попробуй решить задачу, используя только 2 цикла, т.е. будет
+        int size = 5;
+        int tab = size - 1;
+        int symbols = size;
 
-        for (int i = 0; i < *; i++) {
-            for (int j = 0; j < *; j++) {
-                // logic
-            }
-        }
- */
-
-        for (int i = 1; i <= 4; i++) {
-            for (int k = 1; k <= 4 - i; k++) {
+        for (int i = 0; i < size * 2; i++) {
+            for (int k = 0; k < tab; k++) {
                 System.out.print(" ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            for (int j = 1; j <= i; j++) {
+            for (int k = tab; k <= symbols; k++) {
                 System.out.print("*");
             }
             System.out.println();
-        }
-        for (int i = 1; i <= 4; i++) {
-            for (int k = 2; k >= 4 - i; k--) {
-                System.out.print(" ");
+            if (i < size - 1) {
+                tab--;
+                symbols++;
+            } else if (i == size - 1) {
+            } else {
+                tab++;
+                symbols--;
             }
-            for (int j = 4; j >= i; j--) {
-                System.out.print("*");
-            }
-            for (int j = 4; j >= i; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
         }
     }
 }
+
