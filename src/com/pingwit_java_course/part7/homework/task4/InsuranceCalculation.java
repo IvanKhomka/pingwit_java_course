a8d20474d77dc1f948a687ff1c955b49ee67231b
Ivan Khomich <khomichivan03@gmail.com>
2025-02-28 19:56:24 +0100

    Homework 7 tasks 2-4

---
+++ src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
@@ -0,0 +1,24 @@
+package com.pingwit_java_course.part7.homework.task4;
+
+import java.util.Scanner;
+
+public class InsuranceCalculation {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите объем двигателя в см3:");
+        double engineVolume = scanner.nextDouble();
+        System.out.println("Были ли ДТП за прошлый год?(true/false):");
+        boolean crashes = scanner.nextBoolean();
+        System.out.println("Страховались ли у нас?(true/false):");
+        boolean hadInsurance = scanner.nextBoolean();
+
+        double rate = 0.25;
+        double regularCustomerRate = hadInsurance ? 0.8 : 1.0;
+        double crashedCustomerRate = crashes ? 1.2 : 1.0;
+        double totalInsurance = engineVolume * rate * regularCustomerRate * crashedCustomerRate;
+
+        System.out.println("Стоимость вашей страховки составила: " + totalInsurance + " Евро.");
+
+    }
+}
