ddb586fb0b9e0b377565b089559768c6c168d881
Alexander Fomin <rezviybelorus@gmail.com>
2025-03-01 09:44:29 +0000

    [Check] Homework 6,7

--- src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
+++ src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
@@ -7,18 +7,18 @@ public class InsuranceCalculation {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите объем двигателя в см3:");
-        double engineVolume = scanner.nextDouble();
+        double engineVolume = scanner.nextDouble(); // давай добавим здесь абзац. Это не ошибка, но так приятнее будет читать
         System.out.println("Были ли ДТП за прошлый год?(true/false):");
-        boolean crashes = scanner.nextBoolean();
+        boolean crashes = scanner.nextBoolean(); // crashes -> hasCrashes - так сразу понятно что это boolean переменная и давай добавим здесь абзац.
         System.out.println("Страховались ли у нас?(true/false):");
         boolean hadInsurance = scanner.nextBoolean();
 
         double rate = 0.25;
         double regularCustomerRate = hadInsurance ? 0.8 : 1.0;
         double crashedCustomerRate = crashes ? 1.2 : 1.0;
-        double totalInsurance = engineVolume * rate * regularCustomerRate * crashedCustomerRate;
+        double totalInsurance = engineVolume * rate * regularCustomerRate * crashedCustomerRate; // если знаешь про BigDecimal, то лучше через него такие операции делать
 
         System.out.println("Стоимость вашей страховки составила: " + totalInsurance + " Евро.");
-
+// лишняя строка, давай удалим
     }
 }
