5f6500f713d40c127525cca6bf0fff8afd3c6863
Ivan Khomich <khomichivan03@gmail.com>
2025-03-01 12:12:34 +0100

    [Fixed] Homework 7 task 4

--- src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
+++ src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
@@ -1,5 +1,6 @@
 package com.pingwit_java_course.part7.homework.task4;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class InsuranceCalculation {
@@ -7,18 +8,19 @@ public class InsuranceCalculation {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите объем двигателя в см3:");
-        double engineVolume = scanner.nextDouble(); // давай добавим здесь абзац. Это не ошибка, но так приятнее будет читать
+        double engineVolume = scanner.nextDouble();
+
         System.out.println("Были ли ДТП за прошлый год?(true/false):");
-        boolean crashes = scanner.nextBoolean(); // crashes -> hasCrashes - так сразу понятно что это boolean переменная и давай добавим здесь абзац.
+        boolean hasCrashes = scanner.nextBoolean();
+
         System.out.println("Страховались ли у нас?(true/false):");
         boolean hadInsurance = scanner.nextBoolean();
 
         double rate = 0.25;
         double regularCustomerRate = hadInsurance ? 0.8 : 1.0;
-        double crashedCustomerRate = crashes ? 1.2 : 1.0;
-        double totalInsurance = engineVolume * rate * regularCustomerRate * crashedCustomerRate; // если знаешь про BigDecimal, то лучше через него такие операции делать
+        double crashedCustomerRate = hasCrashes ? 1.2 : 1.0;
+        BigDecimal totalInsurance = BigDecimal.valueOf(engineVolume * rate * regularCustomerRate * crashedCustomerRate);
 
         System.out.println("Стоимость вашей страховки составила: " + totalInsurance + " Евро.");
-// лишняя строка, давай удалим
     }
 }
