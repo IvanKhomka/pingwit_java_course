8cd3bea343b0277066c409171b5f242d26f3ef32
Ivan Khomich <khomichivan03@gmail.com>
2025-03-17 15:35:47 +0100

    [Fixed] Homework 7 task 4

--- src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
+++ src/com/pingwit_java_course/part7/homework/task4/InsuranceCalculation.java
@@ -1,6 +1,7 @@
 package com.pingwit_java_course.part7.homework.task4;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class InsuranceCalculation {
@@ -8,7 +9,7 @@ public class InsuranceCalculation {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите объем двигателя в см3:");
-        double engineVolume = scanner.nextDouble();
+        BigDecimal engineVolume = scanner.nextBigDecimal();
 
         System.out.println("Были ли ДТП за прошлый год?(true/false):");
         boolean hasCrashes = scanner.nextBoolean();
@@ -16,12 +17,13 @@ public class InsuranceCalculation {
         System.out.println("Страховались ли у нас?(true/false):");
         boolean hadInsurance = scanner.nextBoolean();
 
-        double rate = 0.25;
-        double regularCustomerRate = hadInsurance ? 0.8 : 1.0;
-        double crashedCustomerRate = hasCrashes ? 1.2 : 1.0;
-        BigDecimal totalInsurance = BigDecimal.valueOf(engineVolume * rate * regularCustomerRate * crashedCustomerRate); // у тебя все переменные должны быть BigDecimal и затем вызываешь метод multiply()
-        // текущий вариант будет работать, но если рейты поменяются, то можешь увидеть неожиданный результат.
-        // попробуй rate = 0.29, engineVolume=1, hasCrashes=true, hadInsurance=false, regularCustomerRate=0.7, crashedCustomerRate=1.3. И параллельно посчитай в калькуляторе
+        BigDecimal rate = new BigDecimal("0.25");
+        BigDecimal regularCustomerRate = hadInsurance ? new BigDecimal("0.8") : BigDecimal.ONE;
+        BigDecimal crashedCustomerRate = hasCrashes ? new BigDecimal("1.2") : BigDecimal.ONE;
+        BigDecimal totalInsurance = engineVolume.multiply(rate)
+                .multiply(regularCustomerRate)
+                .multiply(crashedCustomerRate)
+                .setScale(2, RoundingMode.HALF_UP);
 
         System.out.println("Стоимость вашей страховки составила: " + totalInsurance + " Евро.");
     }
