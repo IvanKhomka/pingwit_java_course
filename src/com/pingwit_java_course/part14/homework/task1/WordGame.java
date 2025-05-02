5cf129eb87ee6d07325865d22c63bfad31f4e7f5
Alexander Fomin <rezviybelorus@gmail.com>
2025-05-02 18:49:08 +0200

    [Check] Homework 14 - merge conflict

--- src/com/pingwit_java_course/part14/homework/task1/WordGame.java
+++ src/com/pingwit_java_course/part14/homework/task1/WordGame.java
@@ -2,7 +2,7 @@ package com.pingwit_java_course.part14.homework.task1;
 
 public class WordGame {
     public static void main(String[] args) {
-        String word = "engineering";
+        String word = "engineering"; //привет от Егорки
         String engine = word.substring(0, 6);
         String ring = word.substring(7);
         System.out.println("First word: " + engine);
