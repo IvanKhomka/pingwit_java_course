6ae45215b45170362739bafd6b2083fa5d215a65
Ivan Khomich <khomichivan03@gmail.com>
2025-07-21 23:37:01 +0200

    [Fixed] Part 26 final mark

--- src/com/pingwit_java_course/part26/homework/task1/Phone.java
+++ src/com/pingwit_java_course/part26/homework/task1/Phone.java
@@ -1,9 +1,4 @@
 package com.pingwit_java_course.part26.homework.task1;
 
 record Phone(String model) {
-    @Override // toString для рекордов переопределен по умолчанию, если ты хотел просто красиво model распечатать, то ок, но в будущем лучше так не делать.
-    // toString обычно для обучения и для логгирования используют, но не как инструмент для работы с данными
-    public String toString() {
-        return model;
-    }
 }
