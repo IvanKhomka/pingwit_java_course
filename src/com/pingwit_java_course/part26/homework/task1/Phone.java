f55600c3c852ace1b4cb4f730c4aba8f9457d270
Alexander Fomin <rezviybelorus@gmail.com>
2025-07-21 20:30:36 +0200

    [Check] Homework 26

--- src/com/pingwit_java_course/part26/homework/task1/Phone.java
+++ src/com/pingwit_java_course/part26/homework/task1/Phone.java
@@ -1,7 +1,8 @@
 package com.pingwit_java_course.part26.homework.task1;
 
 record Phone(String model) {
-    @Override
+    @Override // toString для рекордов переопределен по умолчанию, если ты хотел просто красиво model распечатать, то ок, но в будущем лучше так не делать.
+    // toString обычно для обучения и для логгирования используют, но не как инструмент для работы с данными
     public String toString() {
         return model;
     }
